fbf1491923f0338b97ed6820fb4b5faa83d615f5
Chandan2809 <Chandan2809@users.noreply.github.com>
2026-05-21 13:50:05 +0530

    Initial commit from local workspace

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module RestAssuredJava {
+	requires rest.assured;
+	// hamcrest provides org.hamcrest.Matcher which Rest Assured references
+	requires org.hamcrest;
+}
