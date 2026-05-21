edcecf6d742eec853e2b99eb30e5869efb5a347d
Chandan2809 <Chandan2809@users.noreply.github.com>
2026-05-21 17:16:24 +0530

    Added Payload file

--- src/restassuredtest/BasicsRestAssured.java
+++ src/restassuredtest/BasicsRestAssured.java
@@ -4,6 +4,8 @@ import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import Files.payload;
+
 public class BasicsRestAssured {
 
 	public static void main(String[] args) {
@@ -16,26 +18,11 @@ public class BasicsRestAssured {
        
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		
-		given().log().all().queryParam("Key", "qaclick123").header("Content-Type", "application/json").body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}").
+		given().log().all().queryParam("Key", "qaclick123").header("Content-Type", "application/json").body(payload.AddPlace()).
 		
 		when().post("/maps/api/place/add/json").
 		
-		then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"));
+		then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)");
 	}
 
 }
