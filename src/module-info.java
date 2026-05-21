055578402c3d333c3b930c8d73fa2acfa4b5f6ac
Chandan2809 <Chandan2809@users.noreply.github.com>
2026-05-21 18:06:34 +0530

    Added update place

--- src/module-info.java
+++ src/module-info.java
@@ -8,4 +8,5 @@ module RestAssuredJava {
 	requires rest.assured;
 	// hamcrest provides org.hamcrest.Matcher which Rest Assured references
 	requires org.hamcrest;
+	requires json.path;
 }
--- src/restassuredtest/BasicsRestAssured.java
+++ src/restassuredtest/BasicsRestAssured.java
@@ -1,6 +1,8 @@
 package restassuredtest;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -12,17 +14,42 @@ public class BasicsRestAssured {
 		// TODO Auto-generated method stub
 		// Validate if add place API is working as expected
 		
+		//Add place -> Update place with new address -> Get Place to validate if new address is present in Response  		
 		//Given - all input details
 		//When - Submit the API - resource and http method
 		//Then - validate the response
        
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		
-		given().log().all().queryParam("Key", "qaclick123").header("Content-Type", "application/json").body(payload.AddPlace()).
+		String response = given().log().all().queryParam("Key", "qaclick123").header("Content-Type", "application/json").body(payload.AddPlace()).
 		
 		when().post("/maps/api/place/add/json").
 		
-		then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)");
+		then().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)")
+		.extract().response().asString();
+		
+		
+		System.out.println(response);
+		
+		JsonPath js = new JsonPath(response); // for parsing Json
+		String placedId = js.getString("place_id");
+		System.out.println(placedId);
+		
+		//update place
+		
+		given().log().all().queryParam("key", "quick123").header("Content-Type", "application/json").body("{\r\n"
+				+ "\"place_id\":\""+placedId+"\",\r\n"
+				+ "\"address\":\"70 winter walk, USA\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}").
+		
+		when().put("/maps/api/place/update/json").
+		
+		then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		
+		 //get place
+		
+		
 	}
 
 }
